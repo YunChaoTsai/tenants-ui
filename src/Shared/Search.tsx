4092a6814f5896f250bdc729cf587e4be409b015
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-05-04 23:21:31 +0530

    update(component): update pagination and select component to handle the updated style changes for buttons and input groups

--- src/Shared/Search.tsx
+++ src/Shared/Search.tsx
@@ -28,10 +28,12 @@ export function Search({
       initialValues={initialParams}
       onSubmit={values => onSearch(values)}
       render={() => (
-        <Form noValidate className="display--flex">
-          <InputField name="q" placeholder="Search..." />
-          <Button type="submit">&#128269;</Button>
-          <Button type="reset">&#8634;</Button>
+        <Form noValidate>
+          <div className="input-group">
+            <InputField name="q" placeholder="Search..." />
+            <Button type="submit">&#128269;</Button>
+            <Button type="reset">&#8634;</Button>
+          </div>
         </Form>
       )}
     />
