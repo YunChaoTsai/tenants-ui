c1933717a2b9664841f0023750c9df74dbb9bf83
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-05-10 19:35:43 +0530

    update(layout): let layout component accept the offset and page values for cols

--- src/Shared/Layout.tsx
+++ src/Shared/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Omit } from "utility-types"
+import classNames from "classnames"
 
 export function Container({
   fluid,
@@ -41,26 +42,66 @@ export function Grid({
   )
 }
 
+type NumberAttr = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+type ColSize = true | "auto" | NumberAttr
+type ColBase = { span?: ColSize; offset?: NumberAttr; order?: NumberAttr }
+type ColSpec = ColSize | ColBase
+
+export interface ColProps {
+  xs?: ColSpec
+  sm?: ColSpec
+  md?: ColSpec
+  lg?: ColSpec
+  xl?: ColSpec
+}
+
 export function Col({
   className,
+  xs,
   sm,
   md,
   lg,
   xl,
   as: Component = "div",
   ...props
-}: Omit<React.HTMLProps<HTMLDivElement>, "as"> & {
-  sm?: number | "auto"
-  md?: number | "auto"
-  lg?: number | "auto"
-  xl?: number | "auto"
-  as?: React.ReactType
-}) {
+}: Omit<React.HTMLProps<HTMLDivElement>, "as"> &
+  ColProps & {
+    as?: React.ReactType
+  }) {
+  const prefix = "col"
+  const classes: Array<string> = []
+  const spans: Array<string> = []
+  const deviceSize = [
+    ["xs", xs],
+    ["sm", sm],
+    ["md", md],
+    ["lg", lg],
+    ["xl", xl],
+  ]
+  deviceSize.forEach(([breakPoint, value]) => {
+    let span, offset, order
+    if (value !== null && typeof value === "object") {
+      span = value.span
+      offset = value.offset
+      order = value.order
+    } else {
+      span = value
+    }
+    let infix = breakPoint !== "xs" ? `-${breakPoint}` : ""
+    if (span != null)
+      spans.push(
+        span === true ? `${prefix}${infix}` : `${prefix}${infix}-${span}`
+      )
+
+    if (order != null) classes.push(`order${infix}-${order}`)
+    if (offset != null) classes.push(`offset${infix}-${offset}`)
+  })
+  if (!spans.length) {
+    spans.push("col") // plain 'col'
+  }
   return (
     <Component
-      className={`col ${[["sm", sm], ["md", md], ["lg", lg], ["xl", xl]]
-        .map(([size, value]) => (!value ? "" : `col-${size}-${value}`))
-        .join(" ")} ${className ? className : ""}`}
+      className={classNames(className, ...classes, ...spans)}
       {...props}
     />
   )
