d8eb946dd5818e7e636122e7c7fe5b0f2bd81acf
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-07-22 20:27:20 +0530

    Updates

---
+++ precache-manifest.daf33f179413183dc56bd84782963122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abf9e725d9ba7d37f60a415ae97c36c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2864ccc3f17fe1dabc2",
+    "url": "/static/css/2.246a0412.chunk.css"
+  },
+  {
+    "revision": "2a72b907f8d106a776dd",
+    "url": "/static/css/main.683e26f0.chunk.css"
+  },
+  {
+    "revision": "a2864ccc3f17fe1dabc2",
+    "url": "/static/js/2.93cebaf7.chunk.js"
+  },
+  {
+    "revision": "2a72b907f8d106a776dd",
+    "url": "/static/js/main.512565f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f5b6d6c406793406afe32ad84a5bdd6.js"
+  "/precache-manifest.daf33f179413183dc56bd84782963122.js"
 );
 
 self.addEventListener('message', (event) => {
