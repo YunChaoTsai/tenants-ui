8d46a20d1ddc4422b2cd214045932090dff2e3fc
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-09-03 16:09:36 +0530

    Updates

---
+++ precache-manifest.825b957e6b95be46b103053eee8ae2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ed83e67c5cc32327b253bf91baa282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caf08511182f61ebe92f",
+    "url": "/static/css/2.ae52f51a.chunk.css"
+  },
+  {
+    "revision": "b87def5dfd7a4a1438a3",
+    "url": "/static/css/main.1c145e09.chunk.css"
+  },
+  {
+    "revision": "caf08511182f61ebe92f",
+    "url": "/static/js/2.140017e6.chunk.js"
+  },
+  {
+    "revision": "b87def5dfd7a4a1438a3",
+    "url": "/static/js/main.9de73243.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
