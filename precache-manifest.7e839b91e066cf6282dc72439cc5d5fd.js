3e25f6e21ac9d4fa583c210d6dadacbffd9aeca6
Sudhir Mitharwal <sudhir.mitharwal@tourepedia.com>
2019-09-08 14:18:01 +0530

    Updates

---
+++ precache-manifest.7e839b91e066cf6282dc72439cc5d5fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3332bf047a1a500124a5545f8c1f82da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c56a34cbec781827ca0",
+    "url": "/static/css/2.093e03bd.chunk.css"
+  },
+  {
+    "revision": "61ef49ee70371616152e",
+    "url": "/static/css/main.7f354c35.chunk.css"
+  },
+  {
+    "revision": "4c56a34cbec781827ca0",
+    "url": "/static/js/2.03d8418d.chunk.js"
+  },
+  {
+    "revision": "61ef49ee70371616152e",
+    "url": "/static/js/main.ca94c386.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e09ece6910218f68b747dbdb39ec4ac",
+    "url": "/static/media/undraw_dashboard_nklg.5e09ece6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b765c7760a0087943d3ce5be7254070.js"
+  "/precache-manifest.7e839b91e066cf6282dc72439cc5d5fd.js"
 );
 
 self.addEventListener('message', (event) => {
